933d11c9fdc80f3aedd1be44b25201ad86c7530a
maple <452623741@qq.com>
2017-01-16 17:34:13 +0800

    大大大

---
+++ src/com/jx/background/service/Impl/BgMenuServiceImpl.java
@@ -0,0 +1,255 @@
+package com.jx.background.service.Impl;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.jx.background.config.BgPage;
+import com.jx.common.config.DaoSupport;
+import com.jx.common.config.PageData;
+import com.jx.common.util.MapleDateUtil;
+import com.jx.common.util.UuidUtil;
+import com.jx.background.entity.BgMenu;
+import com.jx.background.service.BgMenuService;
+import com.jx.background.util.BgSessionUtil;
+
+@Service("bgMenuService")
+public class BgMenuServiceImpl implements BgMenuService{
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	
+	/****************************custom * start***********************************/
+	
+	/**
+	 * 根据parentId 获取所有直接fu
+	 * @param String parentId
+	 * @return
+	 * @throws Exception
+	 */
+	public void getParentList(List<BgMenu> parentList, String menuId) throws Exception {
+		parentList.add(0, this.findById(menuId));
+		
+	}
+	
+	/**
+	 * 更改
+	 * @param BgMenu bgMenu
+	 * @throws Exception
+	 */
+	public void changeMenuIcon(BgMenu bgMenu) throws Exception {
+		BgMenu bgMenu1 = new BgMenu();
+		bgMenu1.setMenuIcon(bgMenu.getMenuIcon());
+		bgMenu1.setMenuId(bgMenu.getMenuId());
+		this.change(bgMenu1);
+	}
+	
+	/**
+	 * 根据parentId 获取所有直接子
+	 * @param String parentId
+	 * @return
+	 * @throws Exception
+	 */
+	public List<BgMenu> listByParentId(String parentId) throws Exception {
+		PageData pd = new PageData();
+		pd.put("parentId",parentId);
+		return this.listByPd(pd);
+	}
+	
+	/**
+	 * 获取所有子列表(递归处理)
+	 * @param String menuId
+	 * @return
+	 * @throws Exception
+	 */
+	public List<BgMenu> listInRank(String menuId) throws Exception {
+		List<BgMenu> bgMenuList = this.listByParentId(menuId);
+		for(BgMenu bgMenu : bgMenuList){
+			bgMenu.setSubBgMenuPath("background/menu/list.do?pId="+bgMenu.getMenuId());
+			bgMenu.setSubBgMenuList(this.listInRank(bgMenu.getMenuId()));
+			bgMenu.setTarget("treeFrame");
+		}
+		return bgMenuList;
+	}
+	
+	/**
+	 * 删除所有子列表(递归处理)
+	 * @param String menuId
+	 * @return
+	 * @throws Exception
+	 */
+	public void deleteInRank(String menuId) throws Exception {
+		this.deleteById(menuId);
+		List<BgMenu> bgMenuList = this.listByParentId(menuId);
+		for(BgMenu bgMenu : bgMenuList){
+			this.deleteInRank(bgMenu.getMenuId());
+		}
+	}
+	
+	/**
+	 * 批量删除所有子列表(递归处理)
+	 * @param String dictId
+	 * @return
+	 * @throws Exception
+	 */
+	public void batchDeleteInRank(String[] ids) throws Exception {
+		for(String id : ids){
+			this.deleteInRank(id);
+		}
+	}
+	/****************************custom * end  ***********************************/
+	
+	/****************************common * start***********************************/
+	
+	/**
+	 * 新增 
+	 * @param BgMenu bgMenu
+	 * @throws Exception
+	 */
+	public void add(BgMenu bgMenu) throws Exception {
+		
+		Date nowTime = new Date();
+		bgMenu.setMenuId(UuidUtil.get32UUID());
+		bgMenu.setMenuStatus("00");
+		bgMenu.setEffective("01");
+		bgMenu.setCreateUserId(String.valueOf(BgSessionUtil.getSessionBgUserRole().getUserId()));
+		bgMenu.setCreateTime(nowTime);
+		bgMenu.setModifyUserId(String.valueOf(BgSessionUtil.getSessionBgUserRole().getUserId()));
+		bgMenu.setModifyTime(nowTime);
+		
+		dao.add("BgMenuMapper.add", bgMenu);
+	}
+	
+	/**
+	 * 修改 
+	 * @param BgMenu bgMenu
+	 * @throws Exception
+	 */
+	public void edit(BgMenu bgMenu) throws Exception {
+		Date nowTime = new Date();
+		bgMenu.setModifyUserId(String.valueOf(BgSessionUtil.getSessionBgUserRole().getUserId()));
+		bgMenu.setModifyTime(nowTime);
+		bgMenu.setLastModifyTime(this.findById(bgMenu.getMenuId()).getModifyTime());
+		if(bgMenu.getModifyTime().compareTo(bgMenu.getLastModifyTime()) == 0){
+			bgMenu.setModifyTime(MapleDateUtil.getNextSecond(bgMenu.getModifyTime()));
+		}
+	
+		dao.edit("BgMenuMapper.edit", bgMenu);
+	}
+	
+	/**
+	 * 更改
+	 * @param BgMenu bgMenu
+	 * @throws Exception
+	 */
+	public void change(BgMenu bgMenu) throws Exception {
+		Date nowTime = new Date();
+		bgMenu.setModifyUserId(String.valueOf(BgSessionUtil.getSessionBgUserRole().getUserId()));
+		bgMenu.setModifyTime(nowTime);
+		bgMenu.setLastModifyTime(this.findById(bgMenu.getMenuId()).getModifyTime());
+		if(bgMenu.getModifyTime().compareTo(bgMenu.getLastModifyTime()) == 0){
+			bgMenu.setModifyTime(MapleDateUtil.getNextSecond(bgMenu.getModifyTime()));
+		}
+		dao.edit("BgMenuMapper.change", bgMenu);
+	}
+
+	/**
+	 * 删除 
+	 * @param String menuId
+	 * @throws Exception
+	 */
+	public void deleteById(String menuId) throws Exception {
+		PageData pd = new PageData();
+		pd.put("menuId",menuId);
+		this.deleteByPd(pd);
+	}
+	
+	/**
+	 * 删除 
+	 * @param PageData pd
+	 * @throws Exception
+	 */
+	public void deleteByPd(PageData pd) throws Exception {
+		dao.delete("BgMenuMapper.deleteByPd", pd);
+	}
+	
+	/**
+	 * 批量删除 
+	 * @param PageData pd
+	 * @throws Exception
+	 */
+	public void batchDeleteByIds(String[] ids) throws Exception {
+		dao.delete("BgMenuMapper.batchDeleteByIds", ids);
+	}
+
+	/**
+	 * 通过id获取(类)数据
+	 * @param String menuId
+	 * @return BgMenu
+	 * @throws Exception
+	 */
+	public BgMenu findById(String menuId) throws Exception {
+		PageData pd = new PageData();
+		pd.put("menuId",menuId);
+		return this.findByPd(pd);
+	}
+	
+	/**
+	 * 通过pd获取(BgMenu)数据 
+	 * @param PageData pd
+	 * @return BgMenu
+	 * @throws Exception
+	 */
+	public BgMenu findByPd(PageData pd) throws Exception {
+		return (BgMenu) dao.findForObject("BgMenuMapper.findByPd", pd);
+	}
+	
+	/**
+	 * 获取(类)List数据
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<BgMenu> listByPd(PageData pd) throws Exception {
+		return (List<BgMenu>) dao.findForList("BgMenuMapper.listByPd", pd);
+	}
+	
+	/**
+	 * 获取(类)List数据
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<BgMenu> otherHave(BgMenu bgMenu) throws Exception {
+		return (List<BgMenu>) dao.findForList("BgMenuMapper.otherHave", bgMenu);
+	}
+	
+	/**
+	 * 获取(类)List数据
+	 * @return
+	 * @throws Exception
+	 */
+	public List<BgMenu> otherHaveCode(String menuId, String menuCode) throws Exception {
+		BgMenu bgMenu = new BgMenu();
+		bgMenu.setMenuId(menuId);
+		bgMenu.setMenuCode(menuCode);
+		return this.otherHave(bgMenu);
+	}
+	
+	/**
+	 * 获取分页(PageData)List数据
+	 * @param BgPage BgPage
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listPage(BgPage bgPage) throws Exception {
+		return (List<PageData>) dao.findForList("BgMenuMapper.listPage", bgPage);
+	}
+	
+	/****************************common * end***********************************/
+}
---
+++ src/com/jx/common/tag/ParameterSwitchTag.java
@@ -0,0 +1,178 @@
+package com.jx.common.tag;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.jx.common.entity.ComDict;
+import com.jx.common.service.ComDictService;
+import com.jx.common.util.ServletContextUtil;
+
+public class ParameterSwitchTag extends TagSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private ComDictService comDictService;
+
+	private String id;
+	private String name;
+	private String cssClass;
+	private String styleClass;
+	private String type;
+	private String multiple;
+	private String onChange;
+
+	private String value;
+	private String placeholder;
+	private String title;
+	
+
+	public int doEndTag() throws JspException {
+
+		if (comDictService == null) {
+			comDictService = (ComDictService) ServletContextUtil.getBean("comDictService");
+		}
+		List<ComDict> comDictList = new ArrayList<ComDict>();
+		try {
+			comDictList = comDictService.listSelect(this.getType());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		StringBuffer sb = new StringBuffer();
+		JspWriter out = pageContext.getOut();
+
+		sb.append("<input type=\"checkbox\" name=\"" + this.getName() + "\"");
+
+		if (!StringUtils.isEmpty(this.getId())) {
+			sb.append("id=\"" + this.getId() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getCssClass())) {
+			sb.append("class=\"ace ace-switch " + this.getCssClass() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getStyleClass())) {
+			sb.append("style=\"" + this.getStyleClass() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getMultiple())) {
+			sb.append("multiple=\"" + this.getMultiple() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getOnChange())) {
+			sb.append("onchange=\"" + this.getOnChange() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getPlaceholder())) {
+			sb.append("data-placeholder=\"" + this.getPlaceholder() + "\"");
+		}
+		if (!StringUtils.isEmpty(this.getTitle())) {
+			sb.append("title=\"" + this.getTitle() + "\"");
+		}
+		sb.append(">");
+		sb.append("<option value=\"\">");
+		for (ComDict comDict : comDictList) {
+			if (comDict.getDictValue().equals(this.getValue())) {
+				sb.append("<option value=\"" + comDict.getDictValue() + "\" selected>");
+			} else {
+				sb.append("<option value=\"" + comDict.getDictValue() + "\">");
+			}
+			sb.append(comDict.getDictName() + "</option>");
+		}
+		sb.append("</select>");
+		try {
+			out.write(sb.toString());
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+
+		return TagSupport.EVAL_PAGE;
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCssClass() {
+		return cssClass;
+	}
+
+	public void setCssClass(String cssClass) {
+		this.cssClass = cssClass;
+	}
+
+	public String getStyleClass() {
+		return styleClass;
+	}
+
+	public void setStyleClass(String styleClass) {
+		this.styleClass = styleClass;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getPlaceholder() {
+		return placeholder;
+	}
+
+	public void setPlaceholder(String placeholder) {
+		this.placeholder = placeholder;
+	}
+
+	public String getMultiple() {
+		return multiple;
+	}
+
+	public void setMultiple(String multiple) {
+		this.multiple = multiple;
+	}
+
+	public String getOnChange() {
+		return onChange;
+	}
+
+	public void setOnChange(String onChange) {
+		this.onChange = onChange;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
